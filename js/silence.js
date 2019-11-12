ced08c750ae2633505799b02845263d4bd216fc3
panarowi <49029581+panarowi@users.noreply.github.com>
2019-11-12 18:42:57 +0100

    Update silence.js

--- js/silence.js
+++ js/silence.js
@@ -36,3 +36,21 @@ function update(player) {
     progress.style.width = percent + '%';
     progress.textContent = percent + '%';
 }
+
+function formatTime(time) {
+    var hours = Math.floor(time / 3600);
+    var mins  = Math.floor((time % 3600) / 60);
+    var secs  = Math.floor(time % 60);
+	
+    if (secs < 10) {
+        secs = "0" + secs;
+    }	
+    if (hours) {
+        if (mins < 10) {
+            mins = "0" + mins;
+        }	
+        return hours + ":" + mins + ":" + secs; // hh:mm:ss
+    } else {
+        return mins + ":" + secs; // mm:ss
+    }
+}
